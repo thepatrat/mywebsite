dc5bd64622d41a3fb97b67a909d1b5c73532c696
thepatrat <71664904+thepatrat@users.noreply.github.com>
2022-01-22 18:07:03 +0100

    torus

--- node_main/app.js
+++ node_main/app.js
@@ -1,25 +1,39 @@
 // cube test 
 
+// ~~~~~~~~~ IMPORT ~~~~~~~~
 import * as THREE from 'three';
+import './style.css'
 
+// ~~~~~~~~~ SETUP SCENE ~~~~~~~~~~
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
-
-const geometry = new THREE.TorusGeometry();
-const material = new THREE.MeshBasicMaterial( { color: 0x2F4858} );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
-
+// ~~~~~~~~~ SETUP CAMERA ~~~~~~~~~~~
+const camera = new THREE.PerspectiveCamera(100 , window.innerWidth / window.innerHeight, 0.1, 1000 );
 camera.position.z = 5;
 
+// ~~~~~~~~ SETUP RENDERER ~~~~~~~~~~
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector('#bg')
+});
+
+renderer.setPixelRatio( window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+document.body.appendChild( renderer.domElement );
+
+// ~~~~~~~~ ADD OBJECTS ~~~~~~~~
+const geometry = new THREE.TorusGeometry();
+const material = new THREE.MeshBasicMaterial( { color: 0x2F4858, wireframe: true});
+const cube = new THREE.Mesh( geometry, material );
+
+scene.add( cube );
+
+// ~~~~~~~~ Main Loop ~~~~~~~~
 function animate() {
 	requestAnimationFrame( animate );
 
-	cube.rotation.x += 0.1;
+	cube.rotation.x += 0.01;
+	cube.rotation.y += 0.01;
 
 	renderer.render( scene, camera );
 };
