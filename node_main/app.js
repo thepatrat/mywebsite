a017c107d91ceaceae4a2c9fc1a7115d74d65780
thepatrat <71664904+thepatrat@users.noreply.github.com>
2022-01-22 18:11:17 +0100

    Update app.js

--- node_main/app.js
+++ node_main/app.js
@@ -10,6 +10,7 @@ const scene = new THREE.Scene();
 // ~~~~~~~~~ SETUP CAMERA ~~~~~~~~~~~
 const camera = new THREE.PerspectiveCamera(100 , window.innerWidth / window.innerHeight, 0.1, 1000 );
 camera.position.z = 5;
+camera.position.x = 0;
 
 // ~~~~~~~~ SETUP RENDERER ~~~~~~~~~~
 const renderer = new THREE.WebGLRenderer({
@@ -22,9 +23,12 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild( renderer.domElement );
 
 // ~~~~~~~~ ADD OBJECTS ~~~~~~~~
-const geometry = new THREE.TorusGeometry();
+const geometry = new THREE.IcosahedronGeometry();
 const material = new THREE.MeshBasicMaterial( { color: 0x2F4858, wireframe: true});
 const cube = new THREE.Mesh( geometry, material );
+
+cube.position.y = 3.5;
+cube.position.x = 1;
 
 scene.add( cube );
 
